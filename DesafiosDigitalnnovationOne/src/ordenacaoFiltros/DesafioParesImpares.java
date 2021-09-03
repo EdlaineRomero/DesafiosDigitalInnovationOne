e5fc8144c8bc2f977e2e13934f596e21e99c2318
Edlaine Romero <edlaineromero@gmail.com>
2021-09-02 21:16:56 -0300

    Projeto com os desafios das aulas

---
+++ DesafiosDigitalnnovationOne/src/ordenacaoFiltros/DesafioParesImpares.java
@@ -0,0 +1,38 @@
+package ordenacaoFiltros;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class DesafioParesImpares {
+
+        public static void main(String[] args) throws IOException {
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+            int tamanho = Integer.parseInt(br.readLine());
+
+            List<Integer> list = new ArrayList<Integer>();
+
+            for(int i =0;i<tamanho;i++){
+
+                int valor = Integer.parseInt(br.readLine());
+                list.add(valor);
+            }
+            //continue a solucao de acordo com as notas do enunciado
+            list.stream()
+                    .filter(numero -> numero % 2 == 0)
+                    .sorted()
+                    .forEach(numero -> System.out.println(numero));
+
+            list.stream()
+                    .filter(numero -> numero % 2 != 0)
+                    .sorted(Collections.reverseOrder())
+                    .forEach(numero -> System.out.println(numero));
+
+        }
+
+    }
